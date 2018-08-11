3542916427bd515a9992eae6ac07266bfb084b31
Lu Yang <millerblack@163.com>
2018-08-11 05:23:27 -0700

    Created the database model for Book, now the book data can be added/delted/modifed by superuser.

--- BookManagement/book/models.py
+++ BookManagement/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    book_id = models.IntegerField(default=0)
+    book_name = models.CharField(max_length=200)
+    book_author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.book_name
