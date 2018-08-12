7d3eadb64ff3e5032ef8025733b76e253bd684fd
Lu Yang <millerblack@163.com>
2018-08-12 08:16:38 -0700

    Created templates and modified views for app book, now users can view the true book data from database by website.

--- BookManagement/book/urls.py
+++ BookManagement/book/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'book'
 urlpatterns = [
+            path('', views.index, name='index'),
             path('create', views.create, name='create'),
-            path('read', views.read, name='read'),
+            path('read/<int:book_id>/', views.read, name='read'),
             path('delete', views.delete, name='delete'),
             path('search', views.search, name='search'),
             ]
--- BookManagement/book/views.py
+++ BookManagement/book/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Book
+
+def index(request):
+    all_book_list = Book.objects.order_by('id')[:]
+    template = loader.get_template('book/index.html')
+    context = {
+        'all_book_list': all_book_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def create(request):
     return HttpResponse("create a book ! ")
 
-def read(request):
-    return HttpResponse("read a book ! ")
+def read(request, book_id):
+    try:
+        book = Book.objects.get(pk=book_id)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+    return render(request, 'book/read.html',{'book':book})
 
 def delete(request):
     return HttpResponse("delete a book ! ")
