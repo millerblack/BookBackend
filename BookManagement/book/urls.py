1ad804292e5d042ff654227191baa3bd87096483
Lu Yang <millerblack@163.com>
2018-08-13 08:16:32 -0700

    fix the bug when deleting or reading a book by book_id.

--- BookManagement/book/urls.py
+++ BookManagement/book/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
             path('create/<int:book_id>/<str:book_name>/<str:book_author>/', views.create, name='create'),
             path('read/id/<int:book_id>/', views.read_id, name='read_id'),
             path('read/name/<str:book_name>/', views.read_name, name='read_name'),
-            path('delete/<int:book_id>', views.delete, name='delete'),
-            path('search', views.search, name='search'),
+            path('delete/<int:book_id>/', views.delete, name='delete'),
+            path('search?name=<str:book_name>/', views.search, name='search'),
             ]
--- BookManagement/book/views.py
+++ BookManagement/book/views.py
@@ -26,7 +26,7 @@ def create(request, book_id, book_name, book_author):
 
 def read_id(request, book_id):
     try:
-        book = Book.objects.get(pk=book_id)
+        book = Book.objects.get(book_id=book_id)
     except Book.DoesNotExist:
         raise Http404("Book does not exist")
     return render(request, 'book/read.html',{'book':book})
@@ -40,12 +40,24 @@ def read_name(request, book_name):
 
 def delete(request, book_id):
     try:
-        book = Book.objects.get(pk=book_id)
+        book = Book.objects.get(book_id=book_id)
         book.delete()
     except Book.DoesNotExist:
         raise Http404("Book does not exist")
     return HttpResponse("delete a book ! book_id: %s" % book_id)
 
-def search(request):
-    return HttpResponse("search books ! ")
+def search(request, book_name):
+    all_book_list = Book.objects.order_by('id')[:]
+    include_name_list = []
+    if book_name == "*":
+        include_name_list = all_book_list
+    else:
+        for each_book in all_book_list:
+            if book_name in each_book.book_name:
+                include_name_list.append(each_book)
+    template = loader.get_template('book/index.html')
+    context = {
+        'include_name_list': include_name_list,
+    }
+    return HttpResponse(template.render(context, request))
 
